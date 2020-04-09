dbf551ea70f68389ebfb490bc058cab4e91ee39a
raazkh <serxtdweia9r@gmail.com>
2020-04-09 17:24:43 -0500

    done with chapter 1

---
+++ ch1/Program2.java
@@ -0,0 +1,17 @@
+class Program2
+{
+	static public void main(String[] args)
+	{
+		String p, q;
+		p = new String("hello");
+		q = p.toUpperCase();
+		System.out.println(p);
+		System.out.println(q);
+		String r = new String("bye");
+		String s = "all done";
+		System.out.println(r);
+		System.out.println(s);
+		System.out.println("Writing this program was a waste of time");
+		System.out.println(5/0);
+	}
+}
---
+++ ch1/homework1.java
@@ -0,0 +1,7 @@
+class homework1
+{
+	public static void main(String[] args)
+	{
+		//Wait this is too easy
+	}
+}
